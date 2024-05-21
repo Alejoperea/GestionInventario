6997b57ebeb0a878ed71a000ddddcca3b847b721
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-05-20 19:44:53 -0500

    Creacion Tablas

---
+++ database/migrations/2024_05_21_004002_create_transacciones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transacciones', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipo', ['entrada','salida']);
+            $table->unsignedBigInteger('producto_id');
+            $table->integer('cantidad');
+            $table->date('fecha');
+            $table->timestamps();
+
+            $table->foreign('producto_id')->references('id')->on('productos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transacciones');
+    }
+};
